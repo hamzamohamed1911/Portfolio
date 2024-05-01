9b677e956ca734e1a9dc07ac9ede9e32118680cf
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-01 17:57:56 +0300

    advance it

--- src/app/_components/SideBar.jsx
+++ src/app/_components/SideBar.jsx
@@ -15,37 +15,34 @@ const Sidebar = () => {
     return (
       <div className="fixed top-1/4 right-0 z-20">
 
-        <div className=" mr-1 transition hover:-translate-x-3 duration-400 ">
+        <div className=" mr-1 top-1/4 transition hover:-translate-x-3 duration-300 ">
           {!isOpen && (
             <button onClick={toggleSidebar}><IoIosArrowBack className= " bg-blue-600 text-white rounded-md " size={24} /></button>
           )}
         </div>
     
         <AnimatePresence>
-          {isOpen && (
-           <motion.div
-           initial={{  x: '140%' }}
-           animate={{ x: 0 }}
-           exit={{  x: '140%' }}
-           transition={{ duration: 0.5, type: 'spring', stiffness: 200, damping: 25 }}
-           className= " relative bg-gray-200 shadow-md p-6 rounded-lg">
-          
+  {isOpen && (
+    <motion.div
+      initial={{ x: '140%' }}
+      animate={{ x: 0 }}
+      exit={{ x: '140%' }}
+      transition={{ duration: 0.6, stiffness: 500 }}
+      className="relative bg-gray-200 shadow-md p-6 rounded-lg"
+    >
+      <div className="flex flex-col gap-4 items-center">
+        <button onClick={toggleNightMode}>
+          {dark ? (<FaSun className="text-blue-600 cursor-pointer" size={24} />) : <FaMoon className="text-blue-600 cursor-pointer" size={24} />}
+        </button>
+        <IoMdSettings onClick={() => (setDark(false))} className="text-gray-600 cursor-pointer" size={24} />
+        <button onClick={toggleSidebar} className="absolute bg-blue-600 text-white rounded-md -left-8">
+          <IoIosArrowForward size={24} />
+        </button>
+      </div>
+    </motion.div>
+  )}
+</AnimatePresence>
 
-           <div   className=" flex flex-col gap-4 items-center">
-
-            <button onClick={toggleNightMode}> 
-            {dark ? (<FaSun className="text-blue-600 cursor-pointer" size={24} />) :  <FaMoon className="text-blue-600 cursor-pointer" size={24} />}
-            </button>
-             <IoMdSettings onClick={()=>(setDark(false))}  className="text-gray-600 cursor-pointer" size={24}/> 
-
-             <button onClick={toggleSidebar} className="absolute bg-blue-600 text-white rounded-md -left-8 top-1/4">
-             <IoIosArrowForward size={24} />
-           </button>
-
-           </div>
-         </motion.div>
-          )}
-        </AnimatePresence>
       
       </div>
     );
